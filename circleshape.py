ca9ed8ee7f3f2ef9b6d6905619d2f35aae264700
tytorrez <goncalo.torres@hotmail.com>
2025-10-24 17:25:36 +0100

    Added asteroid collisions with player

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,10 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collide(self, object):
+        distance = self.position.distance_to(object.position)
+        if distance > self.radius + object.radius:
+            return False
+        else:
+            return True
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import *
 from asteroid import *
 from asteroidfield import *
+import sys
 
 def main():
     pygame.init()
@@ -40,6 +41,10 @@ def main():
             drawable.draw(screen)
 
         updatables.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collide(player):
+                sys.exit("Game over!")
 
 
         
